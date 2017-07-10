fb8dec5fd814440360bdc6fe27f105f458abf77c
root <root@Docker-cl0>
2017-07-10 11:50:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+identidock
+==========
+Spasiuk Denys
+From TEST# test1
---
+++ identidock/cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [ "$ENV" = 'DEV' ];
+then
+    echo "SERVER DEV"
+    exec python "identidock.py"
+else
+    echo "SERVER PROD"
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+fi
