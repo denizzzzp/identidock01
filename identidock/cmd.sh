0a9a9765b75075ae254e941a11197381ce948400
root <root@Docker-cl0>
2017-07-19 11:01:25 -0400

    03

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -5,6 +5,10 @@ if [ "$ENV" = 'DEV' ];
 then
     echo "SERVER DEV"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ];
+then
+    echo "SERTER TEST"
+    exec python "tests.py"
 else
     echo "SERVER PROD"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
